166138f9f93979f08b278059a432a13b9b0e1463
ABDALLAH ID LAHCEN <abdallah.idlahcen2@gmail.Com>
2020-12-17 15:00:17 +0100

    first release

---
+++ app/Http/Controllers/create_pivot_user_profile_table.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class create_pivot_user_profile_table extends Controller
+{
+    //
+}
---
+++ database/migrations/2020_12_15_111306_create_images_tabel.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImagesTabel extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('title') ;
+            $table->string('tags');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
